b03ad39a60447c26b5d0694ebe1f4a5a6383356f
reberya <reberya@gmail.com>
2021-08-25 10:09:31 -0400

    updated 'a' variable name

--- code.py
+++ code.py
@@ -1,16 +1,16 @@
 def function1(a):
     b = dict()
-    for c in a:
+    for c in sequence:
         if c not in b:
             b[c] = 1
         else:
             b[c] += 1
     return b
 
-def function2(a):
+def function2(sequence):
     print('freqs')
-    total = float(sum([a[b] for b in a.keys()]))
-    for b in a.keys():
-        print(b + ':' + str(a[b]/total))
+    total = float(sum(sequence[b] for b in sequence.keys()]))
+    for b in sequence.keys():
+        print(b + ':' + str(sequence[b]/total))
 
 function2(function1('ATCTGACGCGCGCCGC'))
